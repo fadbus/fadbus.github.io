53bf71ac234bb9942856b46a9bf4f866dc2bc90c
Derek Nutile <dereknutile@gmail.com>
2015-09-15 16:38:29 -0700

    Adds javascript to build modal dynamically.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,35 @@
 
-// $(function () {
-//   var options = {
-//     valueNames: [ 'font-name', 'font-id', 'font-unicode', 'font-created', 'font-categories' ]
-//   };
-
-//   var fonts = new List('fonts', options);
-// });
-
-
-
 $(document).ready(function(){
 
-    $("[rel=tooltip]").tooltip({
-        animation: true,
-    });
+  $("[rel=tooltip]").tooltip({
+    animation: true,
+  });
 
-    // find filter
-    $('input#font-filter-box').quicksearch('.card-column', {
-        'delay': 500,
-        'bind': 'keyup keydown',
-        'onBefore': function () {
-            clearDisplay();
-        }
-    });
-
-    function clearDisplay () {
-        // $('.something').empty().hide();
+  // find filter
+  $('input#font-filter-box').quicksearch('.card-column', {
+    'delay': 500,
+    'bind': 'keyup keydown',
+    'onBefore': function () {
+      clearDisplay();
     }
+  });
+
+  function clearDisplay () {
+    // $('.something').empty().hide();
+  }
+
+  $('.link-div').click(function(e){
+    var fontId = $(this).data("font-id");
+    var fontName = $(this).data("font-name");
+
+    var fontCode = '<code>&lt;i class="fa fa-' +fontId+ '"&gt;&lt;/i&gt;</code>';
+
+    $("#font-icon-title").html(fontName);
+    $("#icon-code").html(fontCode);
+    $("#fontawesome-url").attr("href", "http://fortawesome.github.io/Font-Awesome/icon/" +fontId+ "/");
+    $('#font-icon').html('<i class="fa fa-' +fontId+ ' fa-5x"></i>');
+
+    $('#font-modal').modal({show:true});
+    e.preventDefault();
+  });
 });
