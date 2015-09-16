d6c1630bed54408796e25b9ca670d54c89aa6835
Derek Nutile <dereknutile@gmail.com>
2015-09-16 14:47:38 -0700

    Pulls icon attributes across views and javascript.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,13 +21,22 @@ $(document).ready(function(){
   $('.link-div').click(function(e){
     var fontId = $(this).data("font-id");
     var fontName = $(this).data("font-name");
+    var fontMeta = $(this).data("font-meta");
+    var fontUnicode = $(this).data("font-unicode");
+    var fontCreated = $(this).data("font-created");
+    var fontCategories = $(this).data("font-categories");
 
-    var fontCode = '<code>&lt;i class="fa fa-' +fontId+ '"&gt;&lt;/i&gt;</code>';
+
+    var iconCode = '<code>&lt;i class="fa fa-' +fontId+ '"&gt;&lt;/i&gt;</code>';
+    var iconMeta = fontMeta;
 
     $("#font-icon-title").html(fontName);
-    $("#icon-code").html(fontCode);
+    $("#icon-code").html(iconCode);
+    $("#font-icon-id").html(fontId);
     $("#fontawesome-url").attr("href", "http://fortawesome.github.io/Font-Awesome/icon/" +fontId+ "/");
     $('#font-icon').html('<i class="fa fa-' +fontId+ ' fa-5x"></i>');
+    $("#font-icon-details").html("Unicode: " +fontUnicode+ " · Created: " +fontCreated+ " · Categories: " +fontCategories);
+    $("#font-icon-meta").html(iconMeta);
 
     $('#font-modal').modal({show:true});
     e.preventDefault();
