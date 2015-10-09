5f126380631e51955b05f02fd8f4d6630d8faaa4
Derek Nutile <dereknutile@gmail.com>
2015-10-09 10:59:37 -0700

    Updates to modal display.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,17 +26,35 @@ $(document).ready(function(){
     var fontCreated = $(this).data("font-created");
     var fontCategories = $(this).data("font-categories");
 
-
     var iconCode = '&lt;i class="fa fa-' +fontId+ '"&gt;&lt;/i&gt;';
     var iconMeta = fontMeta;
+    var iconDetails = "";
+
+    $("#font-icon-meta").empty();
+    $("#font-icon-details").empty();
+
+    if(fontUnicode.length > 0){
+      iconDetails += "<li><b>Unicode:</b>&nbsp;"+fontUnicode+"</li>";
+    }
+
+    if(fontCreated.length > 0){
+      iconDetails += "<li><b>Created:</b>&nbsp;"+fontCreated+"</li>";
+    }
+
+    if(fontCategories.length > 0){
+      iconDetails += "<li><b>Categories:</b>&nbsp;"+fontCategories+"</li>";
+    }
 
     $("#font-icon-title").html(fontName);
     $("#font-icon-code").html(iconCode);
     $("#font-icon-id").html(fontId);
     $("#fontawesome-url").attr("href", "http://fortawesome.github.io/Font-Awesome/icon/" +fontId+ "/");
     $('#font-icon-icon').html('<i class="fa fa-' +fontId+ ' fa-5x"></i>');
-    $("#font-icon-details").html("Unicode: " +fontUnicode+ " · Created: " +fontCreated+ " · Categories: " +fontCategories);
-    $("#font-icon-meta").html(iconMeta);
+    $("#font-icon-details").html(iconDetails);
+
+    if(iconMeta.length > 0){
+      $("#font-icon-meta").html("<b>Meta:</b>&nbsp;"+iconMeta);
+    }
 
     $('#font-modal').modal({show:true});
     e.preventDefault();
